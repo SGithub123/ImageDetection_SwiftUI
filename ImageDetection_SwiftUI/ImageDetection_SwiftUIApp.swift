6af52c7dbc91f98cf82469b315c98d12821e4d03
Sanket <31731312+SGithub123@users.noreply.github.com>
2021-10-16 19:37:03 +0530

    Initial Commit

---
+++ ImageDetection_SwiftUI/ImageDetection_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDetection_SwiftUIApp.swift
+//  ImageDetection_SwiftUI
+//
+//  Created by Sanket Bhamare on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageDetection_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
